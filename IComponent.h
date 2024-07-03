5805282c55ad56466d4d4bc3253b5fed6aac7502
omonnin-ynov <olivier.monnin@ynov.com>
2024-07-03 11:01:36 +0200

    ok

--- IComponent.h
+++ IComponent.h
@@ -3,11 +3,30 @@
 
 namespace LeaderEngine
 {
-	class IComponent {
+    class Entity;
+
+    class IComponent {
+        protected:
+			std::shared_ptr<Entity> parent;
+
 		public:
 			virtual ~IComponent() = default;
 			virtual void Init() = 0;
 			virtual void Update(float deltaTime) = 0;
 			virtual COMPONENT_TYPE GetType() const = 0;
+
+			void setParent(std::shared_ptr<Entity> parent)
+			{
+				this->parent = parent;
+			}
+
+			/**
+			 * \brief WARNING RETURNS NULLPTR IF PARENT NOT ASSIGNED
+			 * \return
+			 */
+			std::shared_ptr<Entity> getParent()
+			{
+				return parent;
+			}
 	};
 }
